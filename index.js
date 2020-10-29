dbc3ee6e6b600f8b0d610ac57883f3dea6e4b176
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-10-29 19:22:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function writeCards(names, event)
   let thankYouArray = [];
   for (let i = 0; i < names.length; i++)
   {
-    finalArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    thankYouArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
   }
   
   return thankYouArray;
