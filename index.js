2fb9648142f4fe69b266e37d7b0077e0eb6c52a6
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-10-29 19:08:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ function writeCards(names, event)
 {
   for (let i = 0; i < names.length; i++)
   {
-    console.log()
+    console.log(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
   }
+  
+  return names;
 }
